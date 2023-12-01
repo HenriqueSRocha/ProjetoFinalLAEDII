76a436593a5851d82d964993093cfc6f152650c9
lllandre <152616087+lllandre@users.noreply.github.com>
2023-12-01 09:32:48 -0300

    Arduino e Salva Arquivos
    Programa do arduino e programa que salva os dados no arquivo

---
+++ serialSalvaArquivo.c
@@ -0,0 +1,113 @@
+#include <windows.h>
+#include <stdio.h>
+#include <time.h>  // Adicionado para usar as funções relacionadas ao tempo
+#include <unistd.h>  // Adicionado para usar a função sleep
+
+int main() {
+    HANDLE hSerial;
+    DCB dcbSerialParams = {0};
+    COMMTIMEOUTS timeouts = {0};
+    FILE *arquivo;
+
+    // Abre a porta serial
+    hSerial = CreateFile("COM4", GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hSerial == INVALID_HANDLE_VALUE) {
+        fprintf(stderr, "Erro ao abrir a porta serial\n");
+        return 1;
+    }
+
+    // Configurações da porta serial
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
+
+    if (!GetCommState(hSerial, &dcbSerialParams)) {
+        fprintf(stderr, "Erro ao obter as configurações da porta serial\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+
+    dcbSerialParams.BaudRate = CBR_9600;  // Velocidade da porta serial (ajuste conforme necessário)
+    dcbSerialParams.ByteSize = 8;         // Tamanho do byte
+    dcbSerialParams.StopBits = ONESTOPBIT; // Um bit de parada
+    dcbSerialParams.Parity   = NOPARITY;   // Sem paridade
+
+    if (!SetCommState(hSerial, &dcbSerialParams)) {
+        fprintf(stderr, "Erro ao configurar a porta serial\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+
+    // Configuração do timeout
+    timeouts.ReadIntervalTimeout         = 50;
+    timeouts.ReadTotalTimeoutConstant    = 50;
+    timeouts.ReadTotalTimeoutMultiplier  = 10;
+    timeouts.WriteTotalTimeoutConstant   = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
+
+    if (!SetCommTimeouts(hSerial, &timeouts)) {
+        fprintf(stderr, "Erro ao configurar timeouts\n");
+        CloseHandle(hSerial);
+        return 1;
+    }
+
+    // Leitura e impressão dos dados
+    char buffer[100];  // Tamanho do buffer ajustável conforme necessário
+    DWORD bytesRead;
+
+    while (1) {
+        arquivo = fopen("saida.txt", "a");
+        if (arquivo == NULL) {
+            fprintf(stderr, "Erro ao abrir o arquivo\n");
+            return 1;
+        }
+
+        // Obtém a data e hora atual
+        time_t tempo_atual;
+        struct tm *info_tempo;
+        time(&tempo_atual);
+        info_tempo = localtime(&tempo_atual);
+
+        if (ReadFile(hSerial, buffer, sizeof(buffer), &bytesRead, NULL)) {
+    if (bytesRead > 0) {
+        // Substitui qualquer caractere de nova linha ou retorno de carro por um espaço em branco
+        for (DWORD i = 0; i < bytesRead; ++i) {
+            if (buffer[i] == '\n' || buffer[i] == '\r' || buffer[i] == ' ') {
+                buffer[i] = ' ';
+            }
+        }
+
+        printf("%.*s- %04d-%02d-%02d %02d:%02d:%02d\n",
+                bytesRead,
+                buffer,
+                info_tempo->tm_year + 1900,
+                info_tempo->tm_mon + 1,
+                info_tempo->tm_mday,
+                info_tempo->tm_hour,
+                info_tempo->tm_min,
+                info_tempo->tm_sec);
+
+        fprintf(arquivo,"%.*s - %04d-%02d-%02d %02d:%02d:%02d\n",
+                bytesRead,
+                buffer,
+                info_tempo->tm_year + 1900,
+                info_tempo->tm_mon + 1,
+                info_tempo->tm_mday,
+                info_tempo->tm_hour,
+                info_tempo->tm_min,
+                info_tempo->tm_sec);
+
+        fclose(arquivo);
+
+    }
+} else {
+            fprintf(stderr, "Erro na leitura da porta serial\n");
+            fclose(arquivo);  // Adicionado para fechar o arquivo em caso de erro
+            break;
+        }
+    }
+
+    // Fecha a porta serial
+    CloseHandle(hSerial);
+
+    return 0;
+}
