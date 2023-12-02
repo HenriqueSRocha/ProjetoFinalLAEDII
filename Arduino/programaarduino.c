fb6b25470bb5de2a694a347a1a874945a1b25fd0
HenriqueSRocha <hsrocha@sga.pucminas.br>
2023-12-02 13:55:01 -0300

    Ajustando sainda do arduino para exibir somente o valor da potência

--- Arduino/programaarduino.c
+++ Arduino/programaarduino.c
@@ -2,7 +2,7 @@
 EnergyMonitor emon1;
 
 //Tensao da rede eletrica
-int rede = 127.0;
+int rede = 110.0;
 
 //Pino do sensor SCT
 int pino_sct = 1;
@@ -18,11 +18,11 @@ void loop() {
   //Calcula a corrente  
   double Irms = emon1.calcIrms(1480);
   //Mostra o valor da corrente
-  //Serial.print("Corrente : ");
-  //Serial.print(Irms); // Irms
+  Serial.print("Corrente : ");
+  Serial.print(Irms); // Irms
 
     //Calcula e mostra o valor da potencia
-  //Serial.print(" Potencia : ");
+  Serial.print(" Potencia : ");
   Serial.println(Irms*rede);
 
    delay(1000);
